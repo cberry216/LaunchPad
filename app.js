c2f36200d41414dd7284dbae9b9ca835efd06db1
Christopher Berry <christopherberry216@gmail.com>
2018-03-19 19:58:08 -0400

    changed ip and port

--- app.js
+++ app.js
@@ -189,7 +189,7 @@ app.get("/about", function(req, res) {
     res.render("contact", {page: "contact"});
 })*/
 
-app.listen(80, '107.180.50.171', function() {
+app.listen(80, '0.0.0.0', function() {
    console.log("Server Started"); 
 });
 
